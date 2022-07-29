80216920e8329cffd199380482fe0618b5ff3370
ozzlopz <ozzlopz@gmail.com>
2022-07-29 08:47:37 -0500

    latest contract address & abi

--- spa-ui/app.js
+++ spa-ui/app.js
@@ -3,263 +3,310 @@ let contractVersion = 0
 
 const abi = [
   {
-    "inputs": [],
-    "name": "contractVersion",
-    "outputs": [
+    anonymous: false,
+    inputs: [
       {
-        "internalType": "string",
-        "name": "",
-        "type": "string"
-      }
+        indexed: false,
+        internalType: "string",
+        name: "_requestName",
+        type: "string",
+      },
     ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
+    name: "ClosedReviewRequest",
+    type: "event",
   },
   {
-    "inputs": [],
-    "name": "reviewFormsTotal",
-    "outputs": [
+    anonymous: false,
+    inputs: [
       {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
+        indexed: false,
+        internalType: "uint256",
+        name: "_formId",
+        type: "uint256",
+      },
     ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
+    name: "CreatedReviewForm",
+    type: "event",
   },
   {
-    "inputs": [
+    anonymous: false,
+    inputs: [
       {
-        "internalType": "string[]",
-        "name": "questions",
-        "type": "string[]"
+        indexed: false,
+        internalType: "string",
+        name: "_requestName",
+        type: "string",
       },
-      {
-        "internalType": "string[][]",
-        "name": "choices",
-        "type": "string[][]"
-      },
-      {
-        "internalType": "enum DeresyRequests.QuestionType[]",
-        "name": "questionTypes",
-        "type": "uint8[]"
-      }
     ],
-    "name": "createReviewForm",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    name: "CreatedReviewRequest",
+    type: "event",
   },
   {
-    "inputs": [
+    anonymous: false,
+    inputs: [
       {
-        "internalType": "string",
-        "name": "_name",
-        "type": "string"
+        indexed: false,
+        internalType: "string",
+        name: "_requestName",
+        type: "string",
       },
-      {
-        "internalType": "address[]",
-        "name": "reviewers",
-        "type": "address[]"
-      },
-      {
-        "internalType": "string[]",
-        "name": "targets",
-        "type": "string[]"
-      },
-      {
-        "internalType": "string[]",
-        "name": "targetsIPFSHashes",
-        "type": "string[]"
-      },
-      {
-        "internalType": "string",
-        "name": "formIpfsHash",
-        "type": "string"
-      },
-      {
-        "internalType": "uint256",
-        "name": "rewardPerReview",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "reviewFormIndex",
-        "type": "uint256"
-      }
     ],
-    "name": "createRequest",
-    "outputs": [],
-    "stateMutability": "payable",
-    "type": "function",
-    "payable": true
+    name: "SubmittedReview",
+    type: "event",
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "string",
-        "name": "_name",
-        "type": "string"
+        internalType: "string",
+        name: "_name",
+        type: "string",
       },
-      {
-        "internalType": "uint8",
-        "name": "targetIndex",
-        "type": "uint8"
-      },
-      {
-        "internalType": "string[]",
-        "name": "answers",
-        "type": "string[]"
-      }
     ],
-    "name": "submitReview",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    name: "closeReviewRequest",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
   },
   {
-    "inputs": [
+    inputs: [],
+    name: "contractVersion",
+    outputs: [
       {
-        "internalType": "string",
-        "name": "_name",
-        "type": "string"
-      }
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
     ],
-    "name": "closeReviewRequest",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    stateMutability: "view",
+    type: "function",
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "string",
-        "name": "_name",
-        "type": "string"
-      }
+        internalType: "string",
+        name: "_name",
+        type: "string",
+      },
+      {
+        internalType: "address[]",
+        name: "reviewers",
+        type: "address[]",
+      },
+      {
+        internalType: "string[]",
+        name: "targets",
+        type: "string[]",
+      },
+      {
+        internalType: "string[]",
+        name: "targetsIPFSHashes",
+        type: "string[]",
+      },
+      {
+        internalType: "string",
+        name: "formIpfsHash",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "rewardPerReview",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "reviewFormIndex",
+        type: "uint256",
+      },
     ],
-    "name": "getRequest",
-    "outputs": [
+    name: "createRequest",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
       {
-        "internalType": "address[]",
-        "name": "reviewers",
-        "type": "address[]"
+        internalType: "string[]",
+        name: "questions",
+        type: "string[]",
       },
       {
-        "internalType": "string[]",
-        "name": "targets",
-        "type": "string[]"
+        internalType: "string[][]",
+        name: "choices",
+        type: "string[][]",
       },
       {
-        "internalType": "string[]",
-        "name": "targetsIPFSHashes",
-        "type": "string[]"
+        internalType: "enum DeresyRequests.QuestionType[]",
+        name: "questionTypes",
+        type: "uint8[]",
+      },
+    ],
+    name: "createReviewForm",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_name",
+        type: "string",
+      },
+    ],
+    name: "getRequest",
+    outputs: [
+      {
+        internalType: "address[]",
+        name: "reviewers",
+        type: "address[]",
       },
       {
-        "internalType": "string",
-        "name": "formIpfsHash",
-        "type": "string"
+        internalType: "string[]",
+        name: "targets",
+        type: "string[]",
       },
       {
-        "internalType": "uint256",
-        "name": "rewardPerReview",
-        "type": "uint256"
+        internalType: "string[]",
+        name: "targetsIPFSHashes",
+        type: "string[]",
       },
       {
-        "components": [
+        internalType: "string",
+        name: "formIpfsHash",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "rewardPerReview",
+        type: "uint256",
+      },
+      {
+        components: [
           {
-            "internalType": "address",
-            "name": "reviewer",
-            "type": "address"
+            internalType: "address",
+            name: "reviewer",
+            type: "address",
           },
           {
-            "internalType": "uint8",
-            "name": "targetIndex",
-            "type": "uint8"
+            internalType: "uint8",
+            name: "targetIndex",
+            type: "uint8",
           },
           {
-            "internalType": "string[]",
-            "name": "answers",
-            "type": "string[]"
-          }
+            internalType: "string[]",
+            name: "answers",
+            type: "string[]",
+          },
         ],
-        "internalType": "struct DeresyRequests.Review[]",
-        "name": "reviews",
-        "type": "tuple[]"
+        internalType: "struct DeresyRequests.Review[]",
+        name: "reviews",
+        type: "tuple[]",
       },
       {
-        "internalType": "uint256",
-        "name": "reviewFormIndex",
-        "type": "uint256"
+        internalType: "uint256",
+        name: "reviewFormIndex",
+        type: "uint256",
       },
       {
-        "internalType": "bool",
-        "name": "isClosed",
-        "type": "bool"
-      }
+        internalType: "bool",
+        name: "isClosed",
+        type: "bool",
+      },
     ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
+    stateMutability: "view",
+    type: "function",
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "uint256",
-        "name": "_reviewFormIndex",
-        "type": "uint256"
-      }
+        internalType: "uint256",
+        name: "_reviewFormIndex",
+        type: "uint256",
+      },
     ],
-    "name": "getReviewForm",
-    "outputs": [
+    name: "getReviewForm",
+    outputs: [
       {
-        "internalType": "string[]",
-        "name": "",
-        "type": "string[]"
+        internalType: "string[]",
+        name: "",
+        type: "string[]",
       },
       {
-        "internalType": "enum DeresyRequests.QuestionType[]",
-        "name": "",
-        "type": "uint8[]"
+        internalType: "enum DeresyRequests.QuestionType[]",
+        name: "",
+        type: "uint8[]",
       },
       {
-        "internalType": "string[][]",
-        "name": "choices",
-        "type": "string[][]"
-      }
+        internalType: "string[][]",
+        name: "choices",
+        type: "string[][]",
+      },
     ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
+    stateMutability: "view",
+    type: "function",
   },
   {
-    "inputs": [],
-    "name": "getReviewRequestsNames",
-    "outputs": [
+    inputs: [],
+    name: "getReviewRequestsNames",
+    outputs: [
       {
-        "internalType": "string[]",
-        "name": "",
-        "type": "string[]"
-      }
+        internalType: "string[]",
+        name: "",
+        type: "string[]",
+      },
     ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
-  }
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "reviewFormsTotal",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_name",
+        type: "string",
+      },
+      {
+        internalType: "uint8",
+        name: "targetIndex",
+        type: "uint8",
+      },
+      {
+        internalType: "string[]",
+        name: "answers",
+        type: "string[]",
+      },
+    ],
+    name: "submitReview",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
 ];
+
 
 let account;
 let web3;
-const contractAddress = "0xD1472423863A07330253eD67d11ADF53322Abc57";
+const contractAddress = "0xc72e3aE0EdB867aF28591686F98Fd9064A78E889";
 
 const handleAccountsChanged = (accounts) => {
   if (accounts.length === 0) {
